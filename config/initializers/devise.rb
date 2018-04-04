a97187bdfb9506489088db694411772e1ff6463a
Vinícius Gregório <vinicius.gregorio@optera.com.br>
2018-04-03 22:14:17 -0300

    fix no devise initializer

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -14,7 +14,7 @@ Devise.setup do |config|
   # ==> Mailer Configuration
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class with default "from" parameter.
-  config.mailer_sender = "victor.amemiya@optera.com.br"
+  config.mailer_sender = "youremail@yourdomain.com"
 
   # Configure the class responsible to send e-mails.
   # config.mailer = "Devise::Mailer"
@@ -240,6 +240,6 @@ Devise.setup do |config|
   # so you need to do it manually. For the users scope, it would be:
   # config.omniauth_path_prefix = "/my_engine/users/auth"
 
-  config.omniauth :google_oauth2, Rails.application.secrets[:google_client_id],
-      Rails.application.secrets[:google_client_secret]
+  # config.omniauth :google_oauth2, Rails.application.secrets[:google_client_id],
+  #     Rails.application.secrets[:google_client_secret]
 end
